883f7486b337d87445d2e120c79feeb87bb99b4d
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-01-19 10:44:52 +0100

    Fix linters errors

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ require './trimmer_decorator'
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :rentals
-  
+
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require './person'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -11,7 +12,7 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.includes?(self)
   end
-  
+
   def play_hooky
     '¯\(ツ)/¯'
   end
